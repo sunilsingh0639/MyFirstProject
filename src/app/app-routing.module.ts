f0eb2f2a5a21620dead65870bbb02a354ef6719f
shankargsamota <sgs786jat@gmail.com>
2023-01-09 11:44:06 +0530

    added class 2 path routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Class1Component } from './class1/class1.component';
+import { Class2Component } from './class2/class2.component';
 
 const routes: Routes = [
-  {path : "class1" , component : Class1Component}
+  {path : "class1" , component : Class1Component},
+  {path : "class2" , component : Class2Component},
 ];
 
 @NgModule({
